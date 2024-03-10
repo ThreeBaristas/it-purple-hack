c79abfd09d65fc3d13ed811d119921ca4221ffea
Egor Fedorov <fegor2004@gmail.com>
2024-03-10 10:06:53 +0300

    backend: add zap logging library

--- backend/go.mod
+++ backend/go.mod
@@ -13,5 +13,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.52.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,20 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/gofiber/fiber/v3"
+	"go.uber.org/zap"
+)
 
 func main()  {
+  logger, _ := zap.NewProduction();
   app := fiber.New()
 
   app.Get("/", func (c fiber.Ctx) error {
+    defer logger.Sync();
+    logger.Info("Hello from / endpoint")
     return c.SendString("Hello there!")
   })
 
+  logger.Info("Starting web server")
   app.Listen(":3000")
 }
