6fd83c273a09f5bee431b336e6e23e15732154b5
Egor Fedorov <fegor2004@gmail.com>
2024-03-10 13:21:05 +0300

    set up frontend with tailwind, prettier, eslint, etc.

---
+++ frontend/src/app/index.tsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { Providers } from './providers'
+
+const container = document.getElementById('root') as HTMLDivElement
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <Providers />
+  </React.StrictMode>
+)
---
+++ frontend/src/app/providers/index.tsx
@@ -0,0 +1,3 @@
+export function Providers() {
+  return <h1 className="text-purple-600">Hello</h1>
+}
