12c8a3892db28e3840d1483bff8080bb068aeb3b
Egor Fedorov <fegor2004@gmail.com>
2024-03-10 15:30:16 +0300

    feat(front): more components, check price form

--- frontend/src/shared/ui/index.ts
+++ frontend/src/shared/ui/index.ts
@@ -1,2 +1,60 @@
-import { Button } from "./button";
+import { Button } from './button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from './card'
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut
+} from './command'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger
+} from './dialog'
+import { Popover, PopoverContent, PopoverTrigger } from './popover'
+
 export { Button }
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
+export {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut
+}
+export { Popover, PopoverContent, PopoverTrigger }
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger
+}
