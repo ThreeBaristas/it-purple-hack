cff9107e52940b766751ed541fe008f0ddd126e0
Egor Fedorov <fegor2004@gmail.com>
2024-03-11 09:13:32 +0300

    merge `categories_repository` and its impl in single file

--- backend/app/repository/categories_repository.go
+++ backend/app/repository/categories_repository.go
@@ -1,8 +1,47 @@
 package repository
 
-import "threebaristas.com/purple/app/models"
+import (
+	"errors"
+	"strings"
+
+	"threebaristas.com/purple/app/models"
+)
 
 type CategoriesRepository interface {
 	GetCategoryByID(id int64) (*models.Category, error)
-  GetByString(s string, n int) ([]*models.Category, error)
+	GetByString(s string, n int) ([]*models.Category, error)
+}
+type CategoriesRepositoryImpl struct {
+	CategoryList []*models.Category
+}
+
+func NewCategoriesRepositoryImpl() *CategoriesRepositoryImpl {
+	return &CategoriesRepositoryImpl{
+		CategoryList: models.GetCategoryListExample(),
+	}
+}
+
+func (r *CategoriesRepositoryImpl) GetCategoryByID(id int64) (*models.Category, error) {
+	list := r.CategoryList
+	for _, category := range list {
+		println(category.ID, category.Name)
+		if category.ID == id {
+			return category, nil
+		}
+	}
+	return nil, errors.New("Not found")
+}
+
+func (r *CategoriesRepositoryImpl) GetByString(s string, max int) ([]*models.Category, error) {
+	list := r.CategoryList
+	var filtered []*models.Category
+	for _, category := range list {
+		if strings.Contains(strings.ToLower(category.Name), strings.ToLower(s)) {
+			filtered = append(filtered, category)
+		}
+		if len(filtered) >= max {
+			break
+		}
+	}
+	return filtered, nil
 }
