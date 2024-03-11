4b59394f32f19cfe095df6ead103ccfae68b30bd
Karabanov Andrey <78631409+forafox@users.noreply.github.com>
2024-03-11 18:24:37 +0300

    test: add tests for GET /categories?search endpoint (#24)

--- backend/test/categories_controller_test.go
+++ backend/test/categories_controller_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
@@ -36,5 +37,75 @@ func TestGetCategoryByID(t *testing.T) {
 		err = json.NewDecoder(resp.Body).Decode(&returnedCategory)
 		assert.NoError(t, err)
 		assert.Equal(t, expectedCategory, &returnedCategory)
+	})
+	t.Run("GetCategoryByID_InvalidID_BadRequest", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/categories/invalid_id", nil)
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	})
+	t.Run("GetCategoryByID_NonExistentID_NotFound", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/categories/999", nil)
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	})
+}
+
+func TestGetCategoriesBySearch(t *testing.T) {
+	expectedCategories := []*models.Category{
+		{ID: 8, Name: "Ноутбуки"},
+	}
+	mockRepo := repository.NewCategoriesRepositoryImpl()
+	controller := controllers.NewCategoriesController(&mockRepo)
+
+	app := fiber.New()
+	app.Get("/categories/search", controller.GetCategoriesBySearch)
+
+	t.Run("Get existing categories by search query", func(t *testing.T) {
+
+		searchQuery := "Ноутбуки"
+
+		req := httptest.NewRequest(http.MethodGet, "/categories/search?search="+searchQuery, nil)
+		resp, err := app.Test(req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		var returnedCategories []*models.Category
+		err = json.NewDecoder(resp.Body).Decode(&returnedCategories)
+		assert.NoError(t, err)
+		assert.ElementsMatch(t, expectedCategories, returnedCategories)
+	})
+
+	t.Run("GetCategoriesByEmptySearch_Success", func(t *testing.T) {
+		// Создание примера дерева локаций
+		locationTree := models.GetCategoryTreeExample()
+
+		req := httptest.NewRequest(http.MethodGet, "/categories/search", nil)
+		resp, err := app.Test(req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		// Чтение тела ответа
+		body, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+
+		var locations []map[string]interface{}
+		err = json.Unmarshal(body, &locations)
+		assert.NoError(t, err)
+
+		// Проверка, что все значения присутствуют в ответе
+		for _, expectedLocation := range locationTree.Children {
+			found := false
+			for _, actualLocation := range locations {
+				if expectedLocation.Name == actualLocation["name"].(string) {
+					found = true
+					break
+				}
+			}
+			assert.True(t, found, "Expected category not found in response: %s", expectedLocation.Name)
+		}
 	})
 }
