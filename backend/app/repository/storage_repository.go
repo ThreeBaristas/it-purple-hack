e3e9bb6aea33ed50bc80b620784dfc73788057ee
Egor Fedorov <fegor2004@gmail.com>
2024-03-12 19:05:24 +0300

    use StorageMapping in PriceService, add SetUpStorage method

--- backend/app/repository/storage_repository.go
+++ backend/app/repository/storage_repository.go
@@ -7,14 +7,36 @@ type MatricesMappingStorage interface {
 
 type InlineMappingStorage struct {
 	baselineMatrix int64
-	mapping        map[int64]int64
+	discounts      map[int64]int64
 }
 
 func (i *InlineMappingStorage) SegmentToMatrix(segmentId int64) (int64, bool) {
-	res, ok := i.mapping[segmentId]
+	if segmentId == 0 {
+		return i.baselineMatrix, true
+	}
+	res, ok := i.discounts[segmentId]
 	return res, ok
 }
 
 func (i *InlineMappingStorage) BaselineMatrix() int64 {
 	return i.baselineMatrix
+}
+
+type DiscountMappingDTO struct {
+	segmentId int64
+	matrixId  int64
+}
+
+type SetUpStorageRequest struct {
+	baselineMatrix int64
+	discounts      []DiscountMappingDTO
+}
+
+func (i *InlineMappingStorage) SetUpStorage(req *SetUpStorageRequest) error {
+	i.baselineMatrix = req.baselineMatrix
+	i.discounts = make(map[int64]int64)
+	for _, mapping := range req.discounts {
+		i.discounts[mapping.segmentId] = mapping.matrixId
+	}
+	return nil
 }
