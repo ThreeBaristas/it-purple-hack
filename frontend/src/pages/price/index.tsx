a619e5d0a2ed7dc1010369a35ed2ccce3bb0fe12
Egor Fedorov <fegor2004@gmail.com>
2024-03-12 15:26:51 +0300

    change priceFormSchema

--- frontend/src/pages/price/index.tsx
+++ frontend/src/pages/price/index.tsx
@@ -1,26 +1,34 @@
+import { valibotResolver } from '@hookform/resolvers/valibot'
 import { createRoute } from '@tanstack/react-router'
-import { number, object, optional, type Output, parse } from 'valibot'
+import { useForm } from 'react-hook-form'
+import { number, object, type Output, parse, partial } from 'valibot'
 
+import { getLocationsQueryOptions } from '@/entities/location/api'
 import { Card, CardContent, CardHeader, CardTitle } from '@/shared/ui'
-import { Label } from '@/shared/ui/label'
 
 import { rootRoute } from '../root'
 import { getPriceQueryOptions } from './api'
 
-export const priceRouteSchema = object({
-  location_id: optional(number()),
-  category_id: optional(number()),
-  segment_id: optional(number()),
-  user_id: optional(number())
+const locationSchema = object({
+  id: number(),
+  name: number()
 })
 
-export type PriceRequest = Output<typeof priceRouteSchema>
+export const priceFormSchema = object({
+  location: locationSchema,
+  category: locationSchema,
+  segment_id: number()
+})
+
+export const priceSearchSchema = partial(priceFormSchema)
+
+export type PriceRequest = Output<typeof priceFormSchema>
 
 export const priceRoute = createRoute({
   getParentRoute: () => rootRoute,
   component: PriceRouteComponent,
   path: '/price',
-  validateSearch: (data) => parse(priceRouteSchema, data),
+  validateSearch: (data) => parse(priceSearchSchema, data),
   loaderDeps: ({ search }) => ({ search }),
   loader: ({ deps, context: { queryClient } }) => {
     return queryClient.ensureQueryData(getPriceQueryOptions(deps.search))
@@ -28,14 +36,9 @@ export const priceRoute = createRoute({
 })
 
 export function PriceRouteComponent() {
-  const {
-    location_id,
-    category_id,
-    user_segment_id: segment_id,
-    matrix_id,
-    price
-  } = priceRoute.useLoaderData()
-  const { user_id } = priceRoute.useSearch()
+  const form = useForm<Output<typeof priceFormSchema>>({
+    resolver: valibotResolver(priceFormSchema)
+  })
 
   return (
     <div>
@@ -43,41 +46,7 @@ export function PriceRouteComponent() {
         <CardHeader>
           <CardTitle>Изменение цены</CardTitle>
         </CardHeader>
-        <CardContent className="space-y-4">
-          <div>
-            <Label>Категория</Label>
-            <p>Категория #{category_id != undefined ? category_id : 'Н/А'}</p>
-          </div>
-          <div>
-            <Label>Локация</Label>
-            <p>Локация #{location_id != undefined ? location_id : 'Н/А'}</p>
-          </div>
-          {user_id != undefined ? (
-            <div>
-              <Label>Пользователь</Label>
-              <p>ID {user_id}</p>
-            </div>
-          ) : null}
-          {segment_id && (
-            <div>
-              <Label>Загружено из сегмента</Label>
-              <p>Сегмент #{segment_id}</p>
-            </div>
-          )}
-          <div>
-            <Label>Загружено из матрицы</Label>
-            <p>Матрица #{matrix_id}</p>
-          </div>
-          <div>
-            <Label>Цена</Label>
-            <p>
-              {new Intl.NumberFormat('ru-RU', {
-                currency: 'RUB',
-                style: 'currency'
-              }).format(price)}
-            </p>
-          </div>
-        </CardContent>
+        <CardContent className="space-y-4"></CardContent>
       </Card>
     </div>
   )
