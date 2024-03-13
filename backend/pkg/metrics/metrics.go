462877afb7998c86adbf6610076cb87793e35bfb
Egor Fedorov <fegor2004@gmail.com>
2024-03-13 23:46:08 +0300

    fix: add segments to users

--- backend/pkg/metrics/metrics.go
+++ backend/pkg/metrics/metrics.go
@@ -10,6 +10,6 @@ var (
   TimeToProcessGetPrice = promauto.NewHistogram(prometheus.HistogramOpts{
     Name: "tb_get_price_request_duration",
     Help: "Time needed to process GET /api/v1/price request (price service endpoint), ms",
-    Buckets: prometheus.LinearBuckets(0, 50, 10),
+    Buckets: prometheus.LinearBuckets(0, 50, 20), // 0..1000
   })
 )
